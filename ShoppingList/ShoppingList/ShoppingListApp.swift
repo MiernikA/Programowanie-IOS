36ada43781d4c2367eb8d750661aa8125f59d02d
Adrian <Adek@Mac.lan>
2026-01-07 13:34:15 +0100

    Zad3-3pkt-create_data_models_in_Core_Data

---
+++ ShoppingList/ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Adrian on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
